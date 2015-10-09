c73e819e1f7aab1cf2d76dacb8037a65cf5083a7
Xiang Li <xl@x1ang.li>
2015-10-09 22:38:33 +0800

    Changed the capitalization of CONSTs so as to improve coding style

---
+++ large-text/replace-str.py
@@ -0,0 +1,19 @@
+import os
+import codecs
+
+"""
+This module is intended for replacing large text file
+"""
+
+base = os.path.join(os.environ['USERPROFILE'], 'Desktop') # User's Desktop, for MicroSoft Windows
+# base = os.path.dirname(__file__)
+fin_name = os.path.join(base, 'Main.asmx.xml')
+fout_name = os.path.join(base, 'output.xml')
+
+
+with codecs.open(fin_name, 'r', 'utf-8') as fin:
+    with codecs.open(fout_name, 'w', 'utf-8') as fout:
+        for linein in fin:
+            lineout = linein.replace('&lt;', '<').replace('&gt;', '>')
+            fout.write(lineout)
+
